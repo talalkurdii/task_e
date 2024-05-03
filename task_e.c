5cdfecf0d2b969110daa50c23cac615e1a64c7cd
Talal.Kurdi <talal.kurdi@student.manchester.ac.uk>
2024-05-03 12:42:53 +0100

    task_e

--- task_e.c
+++ task_e.c
@@ -1,28 +1,40 @@
 #include <stdio.h>
-#include <stdlib.h>
+int main(int argc, char *argv[]){
 
-int main(int argc, char *argv[]) {
     if (argc != 2) {
-        printf("Usage: %s <filename>\n", argv[0]);
+        printf("Error. Expected 1 filename", argv[0]);
         return 1;
     }
-    
+	
+	// Enter your code under here to read the filename from the command line
+	
+	
+	
+	// Read number from file - do not edit
 	double num;
 	FILE *fptr;
-	fptr = fopen("number.txt", "r");
-	if(fptr == NULL) { // display error if can't open file
-        printf("Error. Not able to open the file.");
-		return 1;
-        }
+	fptr = fopen(argv[1], "r");
 	fscanf(fptr, "%lf", &num);
 	fclose(fptr);
 	
+
+	// Enter your code from Lab D to print JSON output (with name, email, number1, number2) goes under here
+    
+    
+    if (fptr == NULL ){
+        printf("Error. Cannot open requested file", argv[0]);
+        return 1;
+    }
+    
+    
     printf("{"); 
     printf("\"name\": \"Talal Kurdi\",\n"); 
     printf("\"email\": \"talal.kurdi@student.manchester.ac.uk\",\n"); 
     printf("\"number1\": %.2f,\n", num); 
     printf("\"number2\": %.3e}\n", num);
 	
+	
+
 	// Do not edit below here
 	return 0;
 }
