387d91c0c55be41ac0206d259c712e39980aac62
Talal.Kurdi <talal.kurdi@student.manchester.ac.uk>
2024-05-03 12:29:00 +0100

    task_e

--- task_e.c
+++ task_e.c
@@ -6,23 +6,23 @@ int main(int argc, char *argv[]) {
         printf("Usage: %s <filename>\n", argv[0]);
         return 1;
     }
-
-    double num;
-    FILE *fptr;
-
-    fptr = fopen(argv[1], "r");
-    if (fptr == NULL) {
-        printf("Error. Not able to open the file.\n");
-        return 1;
-    }
-
-    fscanf(fptr, "%lf", &num);
-    fclose(fptr);
-
-    printf("name:  Talal Kurdi\n");
-    printf("email: talal.kurdi@student.manchester.ac.uk\n");
-    printf("number1: %.2lf,\n", num);
-    printf("number2: %.3e\n", num);
-
-    return 0;
+    
+	double num;
+	FILE *fptr;
+	fptr = fopen("number.txt", "r");
+	if(fptr == NULL) { // display error if can't open file
+        printf("Error. Not able to open the file.");
+		return 1;
+        }
+	fscanf(fptr, "%lf", &num);
+	fclose(fptr);
+	
+    printf("{"); 
+    printf("\"name\": \"Talal Kurdi\",\n"); 
+    printf("\"email\": \"talal.kurdi@student.manchester.ac.uk\",\n"); 
+    printf("\"number1\": %.2f,\n", num); 
+    printf("\"number2\": %.3e}\n", num);
+	
+	// Do not edit below here
+	return 0;
 }
